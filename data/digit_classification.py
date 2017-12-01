154dfdaacc9a26e9ea986a7909bf1434f5d39c45
charlesma4 <charlesma4@gmail.com>
2017-12-01 17:08:05 -0500

    setting up single digit classification

--- data/digit_classification.py
+++ data/digit_classification.py
@@ -12,3 +12,8 @@ num_pixels = X_train.shape[1] * X_train.shape[2]
 X_train = X_train.reshape(X_train.shape[0], num_pixels).astype('float32')
 X_test = X_test.reshape(X_test.shape[0], num_pixels).astype('float32')
 
+X_train /= 255
+X_test /= 255
+
+y_train = np.utils.to_categorical(y_train)
+y_test = np.utils.to_categorical(y_test)
