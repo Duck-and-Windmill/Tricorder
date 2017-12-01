52fe3d04910171d2b7252b3ff66f5de6ea1af0ad
burstein <burstein@bursteins-MacBook-Pro.local>
2017-12-01 16:55:42 -0500

    Got template

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-import Flask 
+from flask import Flask, render_template, request
+import json
+import os
+
+template_dir = os.path.abspath('static')
+app = Flask(__name__, template_folder=template_dir)
+
+@app.route('/')
+def main():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
