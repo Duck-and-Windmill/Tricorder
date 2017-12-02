758c346dcf92892b2d43b66788a8a8ec14bf492a
burstein <burstein@bursteins-MacBook-Pro.local>
2017-12-02 10:32:08 -0500

    I dont know if this will work

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import json
 import os
+import base64 
 
 template_dir = os.path.abspath('static')
 app = Flask(__name__, template_folder=template_dir)
@@ -9,5 +10,14 @@ app = Flask(__name__, template_folder=template_dir)
 def main():
     return render_template("index.html")
 
+@app.route('/sendStaticImage',  methods=["POST"])
+def sendStaticImage():
+	image64=request.form['image']
+	image=base64.decodestring(image_64_encode)
+	print(image)
+	fh = open("test.png", "wb")
+	fh.write(image)
+	fh.close()
+
 if __name__ == "__main__":
-    app.run()
+    app.run()	
