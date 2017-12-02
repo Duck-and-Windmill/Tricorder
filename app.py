8d459ea12b2d77ae05742cbccd256b8c904f9a5b
Jinghu Lei <jinghul.lei@gmail.com>
2017-12-02 12:11:35 -0500

    asdok

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request
 import json
 import os
-import base64 
-import data.classifier
 
 template_dir = os.path.abspath('static')
 app = Flask(__name__, template_folder=template_dir)
@@ -11,22 +9,5 @@ app = Flask(__name__, template_folder=template_dir)
 def main():
     return render_template("index.html")
 
-@app.route('/sendStaticImage',  methods=["POST"])
-def sendStaticImage():
-
-	# with open("data/img/banana.jpg", "rb") as image_file:
-		# encoded_string = base64.b64encode(image_file.read())
-
-	image64=request.form['image']
-	image64=encoded_string
-	image=base64.decodestring(image64)
-	print(image)
-	
-	fh = open("test.png", "wb")
-	fh.write(image)
-	fh.close()
-	classifier.model("test.png")
-
 if __name__ == "__main__":
-	# sendStaticImage()
-	app.run()	
+    app.run()
