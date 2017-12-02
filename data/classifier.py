0f1b14b79322e1f58a6554dc64c6f81712314311
charlesma4 <charlesma4@gmail.com>
2017-12-01 23:06:07 -0500

    woo

--- data/classifier.py
+++ data/classifier.py
@@ -1,10 +1,11 @@
 import numpy as np
+import vgg16
 
 class model():
 
 	def __init__(self, image):
 
-		self.model = VGG16(weights="imagenet")
-		self.preds = self.model.predict(preprocess_input(imafge))
+		self.model = vgg16(weights="imagenet")
+		self.preds = self.model.predict(preprocess_input(image))
 		print(decode_predictions(self.preds))
 
